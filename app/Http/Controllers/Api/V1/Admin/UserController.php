be27aeb2482135f62b0e8d984aa19e3905e9f752
Marcelo Camargo <m.camargo@reply.com>
2020-10-04 20:02:47 -0300

    Criado as APIs de Usuários, Funções e Permissões

---
+++ app/Http/Controllers/Api/V1/Admin/UserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+use Spatie\Permission\Models\Role;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Gate;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use Spatie\Permission\Models\Permission;
+use App\Http\Resources\Admin\UserResource;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        abort_if(Gate::denies('user_access'), Response::HTTP_FORBIDDEN);
+        return new UserResource(User::with(['roles', 'permissions'])->get());
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        $user = User::create($request->except(['roles', 'permissions']));
+        $user->syncRoles($request->input('roles', []));
+        $user->syncPermissions($request->input('permissions', []));
+
+        return (new UserResource($user))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    public function show(User $user)
+    {
+        abort_if(Gate::denies('user_show'), Response::HTTP_FORBIDDEN);
+        return new UserResource($user->load(['roles', 'permissions']));
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        // para não alterar a senha se estiver vazia
+        if (is_null($request['password'])) {
+            unset($request['password']);
+        }
+
+        $user->update($request->except(['roles', 'permissions']));
+        $user->syncRoles($request->input('roles', []));
+        $user->syncPermissions($request->input('permissions', []));
+
+        return (new UserResource($user))
+            ->response()
+            ->setStatusCode(Response::HTTP_ACCEPTED);
+        
+    }
+
+    public function destroy(User $user)
+    {
+        abort_if(Gate::denies('user_delete'), Response::HTTP_FORBIDDEN);
+        $user->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:api'], 'prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin'], function () {
+    Route::apiResource('users', App\Http\Controllers\Api\V1\Admin\UserController::class);
+    Route::apiResource('roles', App\Http\Controllers\Api\V1\Admin\RoleController::class);
+    Route::apiResource('permissions', App\Http\Controllers\Api\V1\Admin\PermissionController::class);
 });
