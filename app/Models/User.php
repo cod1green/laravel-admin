80332c7b287f9e0f14dad099747c068598959993
Marcelo Camargo <m.camargo@reply.com>
2020-12-04 17:31:31 -0300

    Adicionado a coluna active no $fillable da model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable implements HasMedia
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'avatar_id',
+        'name', 'email', 'password', 'avatar_id', 'active'
     ];
 
     protected $appends = ['registered'];
