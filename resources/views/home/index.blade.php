fccde9d7deb68ba58d2fe9e579ebdb64869865c8
Marcelo Camargo <celaummm@gmail.com>
2021-04-06 20:43:20 -0300

    Alterado a home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,29 +1,14 @@
 @extends('layouts.app')
 @section('title', 'Home')
+
 @section('content')
 
 <div class="container">
     @include('includes.alerts')
 
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <h1><i class='fa fa-wrench'></i> Em Construção...</h1>
-                </div>
-
-                <div class="card-body">
-                    <h4><b>Usuários para teste</b></h4>
-                    <hr>
-                    <h5>ADMIN</h5>
-                    <h5><b>Usuario:</b> admin@admin.com</h5>
-                    <h5><b>Senha:</b> 12345678</h5>
-                    <hr>
-                    <h5>USER</h5>
-                    <h5><b>Usuario:</b> user@user.com</h5>
-                    <h5><b>Senha:</b> 12345678</h5>
-                </div>
-            </div>
+        <div class="col-md-12 text-center">
+            <h1>Em construção...</h1>
         </div>
     </div>
 </div>
@@ -42,12 +27,6 @@
         background-position: center;
         background-repeat: no-repeat;
         background-size: auto 100%;
-    }
-
-    main .container {
-        width: 500px;
-        opacity: 0.8;
-        filter: alpha(opacity=80);
     }
 </style>
 @endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -34,7 +35,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -47,58 +50,61 @@
                             </a>
                         </li>
 
+                        @auth
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('admin.home') }}">
                                 <i class="fa fa-btn fa-tachometer-alt"></i> Painel
                             </a>
                         </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    @if(Auth::user()->avatar)
-                                        <img src="{{ Auth::user()->avatar->getUrl('thumb') }}" width="25" height="25"
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                @if(Auth::user()->avatar)
+                                <img src="{{ Auth::user()->avatar->getUrl('thumb') }}" width="25" height="25"
                                     alt="avatar" class="rounded-circle">
-                                    @else
-                                        <img src="{{ asset('img/no-user.png') }}" width="25" height="25" alt="avatar" class="rounded-circle">
-                                    @endif
+                                @else
+                                <img src="{{ asset('img/no-user.png') }}" width="25" height="25" alt="avatar"
+                                    class="rounded-circle">
+                                @endif
 
-                                    {{ Auth::user()->name }}
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('profile.edit') }}">
+                                    <i class="fa fa-btn fa-user"></i> @lang('admin.profile')
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('profile.edit') }}">
-                                        <i class="fa fa-btn fa-user"></i> @lang('admin.profile')
-                                    </a>
+                                <a class="dropdown-item" href="{{ route('admin.home') }}">
+                                    <i class="fa fa-btn fa-tachometer-alt"></i> Painel
+                                </a>
 
-                                    <a class="dropdown-item" href="{{ route('admin.home') }}">
-                                        <i class="fa fa-btn fa-tachometer-alt"></i> Painel
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        <i class="fas fa-power-off"></i> {{ __('Logout') }}
-                                    </a>
+                                    <i class="fas fa-power-off"></i> {{ __('Logout') }}
+                                </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -116,4 +122,5 @@
     @yield('js')
     @stack('js')
 </body>
+
 </html>
